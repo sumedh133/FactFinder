fd7739e06ac0ac20551c1013a2e28e1431b86465
sumedh133 <sumedh133@gmail.com>
2024-12-05 17:35:30 +0530

    bugs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from waitress import serve
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS  
 from utilities.chatbot import get_response  
@@ -30,4 +29,4 @@ def misinfo_chatbot():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    serve(app,host="0.0.0.0", port=5000)
+    app.run(app, port=5000)
